dc76ce6bd7fa4f198f7a8c4f59de9697e74c18ba
Jonathan Allen <jallen@snow.edu>
2022-09-09 09:20:54 -0600

    finished challenges for ch 6

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ushort score = 5;
+Console.WriteLine($"You scored {score} points (ushort)");
+
+float otherScore;
+otherScore = 1.2F;
+Console.WriteLine($"Your other score is {otherScore} (float)");
+
+
+//....do that for all data types
+
+//challenge 2 - modify the values
+score = 10;
+Console.WriteLine("The new score is " + score + "(ushort)");
+
+//..do that for other data types
+
+
+
+
